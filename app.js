a82cbf6a991faeb1067d3dca09ebee4bca86acbe
Zachary Morel <morelz133@gmail.com>
2017-08-31 16:59:35 -0400

    wokring on my authentication working in my Login routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./middleware/login')
 
 var app = express();
 
@@ -22,8 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
+app.use('/api/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ middleware/login.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const models = require('./../models')
+
+// login Passport Middleware goes here
+passport.use("login", new LocalStrategy(username, password, next) => {
+            models.Users
+                .findOne({
+                        where: {
+                            username: username
+                        })
+                    .then(user => {
+                        // 
+                    })
+                })
+            })
