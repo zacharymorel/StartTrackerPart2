f97ece8b906fab5c0258e2199e77f6c688285cce
Zachary Morel <morelz133@gmail.com>
2017-09-20 13:43:10 -0400

    got my activites to read correctly

--- routes/userActivities.js
+++ routes/userActivities.js
@@ -11,13 +11,15 @@ module.exports = (userActivities) => {
                     UserId: req.session.passport.user
                 }
             }).then(ActivitiesDones => {
-                console.log('1', ActivitiesDones)
+
+                // ActivitiesDones[0].getActivity().then(activity => console.log('1.75', activity.ActivityName))
+                // ^^ this is the fancy way to get and set that the ORM is using to create tasks and query tasks based off of associations
                 models.Activities.findAll({
                     where: {
-                        id: ActivitiesDones.dataValues.ActivityId
+                        id: {$in: ActivitiesDones.map(x => x.ActivityId)} 
                     }
                 }).then(myActivities => {
-                    console.log('2', myActivities)
+                    // console.log('2', myActivities)
                     res.render('userActivity', {
                         ActivitiesDones: ActivitiesDones,
                         myActivities: myActivities
@@ -31,28 +33,46 @@ module.exports = (userActivities) => {
     })
 
     // FIND THE SPECIFIC ACTIVITY BY IT'S ACTIVITYID AND RENDER SPECIFIC PAGE
-    router.get(`/useractivity/:ActivityId`, (req, res) => {
-        const id = req.params.ActivityId
+    router.get(`/useractivity/:Id`, (req, res) => {
+        const id = req.params.Id
         const activityid = parseInt(id)
 
         models.ActivitiesDones.findOne({
             where: {
                 ActivityId: activityid
-            },
-            raw: true
-        }).then(activityInfo => {
-            const activity = {
-                title: req.user.username,
-                ActivityId: activityInfo.ActivityId,
-                Count: activityInfo.Count,
-                DateCompleted: activityInfo.DateCompleted,
-                createdAt: activityInfo.createdAt,
-                updatedAt: activityInfo.updatedAt
             }
-            res.render('specificUserActivity', activity)
+        }).then(ActivitiesDones => {
+            // console.log('2', ActivitiesDones)
+            models.Activities.findAll({
+                where: {
+                    id: ActivitiesDones.ActivityId
+                }
+            }).then(myActivities => {
+                // console.log('3', myActivities)
+                // console.log('2.5', myActivities[0].dataValues.ActivityName)
+                // const ActivityNameValue =  myActivities[0].dataValues.ActivityName
+                // console.log('9', ActivityNameValue)
+                const activity = {
+                    ActivityName: myActivities[0].dataValues.ActivityName,
+                    Count: ActivitiesDones.Count,
+                    DateCompleted: ActivitiesDones.DateCompleted,
+                    createdAt: ActivitiesDones.createdAt,
+                    updatedAt: ActivitiesDones.updatedAt
+                }
+                res.render('specificUserActivity', activity)
+            })
+        }) 
+        .catch(err => {
+            console.log('3', err)
+            res.redirect('/')
         })
     })
 
+    // parent.map( (eachActivity, index) => {
+    //     eachActivity[index].datavalue.ActivyName
+    //     console.log(eachActivity[index].datavalues.ActivyName)
+    // })
+    
     // UPDATING USER SPECIFIC ACTIVITY
     router.post('/api/activities/:id/update', (req, res) => {
         const id = parseInt(req.body.ActivityId)
